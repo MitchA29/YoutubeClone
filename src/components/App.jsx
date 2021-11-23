8456ea45341fe7d9435911bfa9d61598b6dcf662
andrew morales <andrew.morales725@gmail.com>
2021-11-23 16:19:27 -0500

    Added search bar functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/TitleBar';
 import SearchBar from './SearchBar/SearchBar';
+import axios from 'axios';
 
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = [
-        ];
+        this.state = {
+            videos: []
+    }
+}
+
+    getVideoList = async (searchFor) => {
+        let response = await axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyAUFW6W2O6Mqz3liLuFKlGvg4H4ITggyGA&kind="video"&part=snippet&q=' + {searchFor})
+        console.log(response.data.items)
+        this.setState({
+            videos: response.data.items
+        })
     }
 
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <SearchBar />
+                <SearchBar getVideoList={this.getVideoList}/>
             </div>
         )
     }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import "./SearchBar.css"
 import 'bootstrap/dist/css/bootstrap.css';
 
-function SearchBar(props){
+const SearchBar = (props) => {
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        let searchFor = document.getElementById('searchFor').value;
+        console.log(searchFor);
+        props.getVideoList(searchFor);
+    }
+
     return (
         <div class="searchBarParent">
             <div class="searchField">
             <nav class="navbar navbar-light bg-light">
                 <div class="container-fluid">
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button type="button" class="btn btn-danger">Search</button>
+                    <form onSubmit={(event) => handleSubmit(event)} class="d-flex">
+                        <input id="searchFor" class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button type="submit" class="btn btn-danger">Search</button>
                     </form>
                     </div>
             </nav>
